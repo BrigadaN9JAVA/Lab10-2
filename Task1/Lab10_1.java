abb062f40449899c583394adec85f24bb39cc7a7
MrZend <vlad04102002@gmail.com>
2021-05-12 14:50:28 +0300

    Second Commit

--- Task1/Lab10_1.java
+++ Task1/Lab10_1.java
@@ -2,9 +2,13 @@ package Task1;
 
 public class Lab10_1 {
 
+    static void result(FunctionalLab10 formula, double a, double b, double c, double d){
+        System.out.println("> a => " + a + "; b => " + b + "; c => " + c + "; " + " d => " + d + ";");
+        System.out.println("> Result => " + formula.formula(a, b, c, d));
+    }
+    
     public static void main(String[] args) {
         FunctionalLab10 formula = (a, b, c, d) -> (Math.pow(a, b) / Math.cosh(Math.abs(b))) + ((4 * Math.log10(c)) / Math.pow(d, 0.2));
-        System.out.println("> a => " + 2.98 + "; b => " + 5.55 + "; c => " + 0.045 + "; " + " d => " + 0.129 + ";");
-        System.out.println("> Result => " + formula.formula(2.98, 5.55, 0.045, 0.129));
+        result(formula, 2.98, 5.55, 0.045, 0.129);
     }
 }
